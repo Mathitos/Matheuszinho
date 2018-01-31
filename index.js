960f2869aa2b6ea1267d903ce72e6e9e94ec9fac
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2018-01-31 20:57:31 -0200

    trying to add roll functionality

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-console.log("oi");
+console.log("Oi, estou pronto para ajudar meus amiguinhos!!!");
 const Discord = require('discord.js');
 const Matheuszinho = new Discord.Client();
+const comand_key = '!';
 
 Matheuszinho.on('message', (message) => {
 
-  if(message.content == 'ping'){
-    message.channel.send('pong!');
+  if(message.content[0] == comand_key){
+    text_content = message.content.substring(1,message.content.lenght);
+    if(text_content == 'ping'){
+      message.channel.send('pong!');
+    } else
+    if(text_content == 'roll d20'){
+      message.channel.send(Math.floor(Math.random() * 20));
+    }
+
+
+
   }
 });
 
